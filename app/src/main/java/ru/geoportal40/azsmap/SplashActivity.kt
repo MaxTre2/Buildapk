c72d2e51908a301598fe244f81ffbaabb2b30439
MaxTre2 <40918814+MaxTre2@users.noreply.github.com>
2026-08-21 14:06:58 +0300

    Create SplashActivity.kt

---
+++ app/src/main/java/ru/geoportal40/azsmap/SplashActivity.kt
@@ -0,0 +1,24 @@
+package ru.geoportal40.azsmap
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_DURATION_MS = 1800L
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DURATION_MS)
+    }
+}
